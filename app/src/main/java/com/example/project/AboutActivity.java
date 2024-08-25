9c1d45b00d30df913f5cc303220b40f3d899fd84
Simon Rydberg <simonrydberg@hotmail.com>
2024-08-25 22:31:05 +0200

    about page

---
+++ app/src/main/java/com/example/project/AboutActivity.java
@@ -0,0 +1,20 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        WebView myWebView;
+        myWebView = findViewById(R.id.about_webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("file:///android_asset/about.html");
+    }
+}
